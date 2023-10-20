1624701c4acbd25551435147b6796adb29f26efe
Moon-Elf <mauryaumang03@gmail.com>
2023-10-20 20:29:12 +0530

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; // trat all JS code as newer version
+
+// alert("Hello") // we are using nodejs, not browser
+
+console.log(3
+    +
+    3) //code readablity should be high
+
+console.log('Umang')
+
+
+let name = "umang"
+let age  = 20
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol
+
+
+// object
+
+console.log(typeof undefined);
+console.log(typeof null);
