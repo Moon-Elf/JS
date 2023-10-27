27ad13547dda345247dda7111fb47f04f2e4f395
Moon-Elf <mauryaumang03@gmail.com>
2023-10-27 18:39:04 +0530

    String in JavaScript

---
+++ 01_basics/05_string.js
@@ -0,0 +1,24 @@
+const name = 'Umang'
+const repoCount = 10
+
+// console.log(`Hi my name is ${name}, i have ${repoCount} on github.`)
+
+const gameName = new String('umangmaurya--compb')
+
+// console.log(gameName[2]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('m'));
+
+const newString = gameName.substring(0, 4)
+// console.log(newString);
+
+const anotherString = gameName.slice(-2, 1)
+// console.log(anotherString);
+
+const newStringOne = "        umang      "
+console.log(newStringOne);
+console.log(newStringOne.trim());
