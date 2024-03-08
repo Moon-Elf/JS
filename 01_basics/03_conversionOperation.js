2652d857a314e0ee7878063c07f2709dad4ab594
Moon-Elf <mauryaumang03@gmail.com>
2024-03-08 23:04:21 +0530

    operations & prefix &  postfix

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -29,5 +29,29 @@ let someNumber = 33
 
 let stringNumber = String(someNumber)
 
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//! console.log(stringNumber);
+//! console.log(typeof stringNumber);
+
+
+// ******************************** Operation ************************ 
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Hello"
+let str2 = " Umang"
+let str3 = str1 + str2
+// console.log(str3);
+
+let gameCounter = 100
+// gameCounter++
+++gameCounter
+// console.log(gameCounter);
