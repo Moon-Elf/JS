b28a70bbdbd3ae9d9779947e8f4aa83ee778bb34
Moon-Elf <mauryaumang03@gmail.com>
2024-03-08 22:37:49 +0530

    datatypes

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,7 +2,14 @@ const accountId = 12342
 let accountEmail = "test@gmail.com"
 var accountPassword = "12345"
 accountCity = "Mumbai"
+let accountState;
 
-accountId = 2
 
 console.log(accountId);
+
+/*
+    Prefer not to use var
+    because of functional scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all JS code as newer version
+
+// alert("hello") // we are using Node.js not browser
+
+// console.log(3 + 3)
+
+// console.log("Umang")
+
+// number => 2 ^ 53
+// bigint
+// string => ""
+// boolean => true, false
+// null => standalone value
+// undefined 
+// symbol => unique identifier
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
