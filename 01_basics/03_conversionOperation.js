e6070bd315e202e42f9564a58af11d8b6531d63e
Moon-Elf <mauryaumang03@gmail.com>
2024-03-08 22:49:07 +0530

    conversion operations

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,33 @@
+// let score = "33"
+// let score = "33a"
+// let score = null
+// let score = undefined
+let score = true
+
+//! console.log(typeof score);
+
+let valueInNumber = Number (score)
+
+//! console.log(valueInNumber);
+
+//! console.log(typeof valueInNumber);
+
+// "33" => 33
+// "33ab" => Nan
+// true => 1 || false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// ! console.log(booleanIsLoggedIn);
+
+// 1 => true || 0 => false
+// "" => false || "umang" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
