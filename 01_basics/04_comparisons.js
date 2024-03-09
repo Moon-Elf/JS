23ebad77565c08c806f72b902b6686a27a28fec9
Moon-Elf <mauryaumang03@gmail.com>
2024-03-09 11:19:11 +0530

    datatypes summary

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,24 @@
+/* 
+ rule   less than operator (<)
+ rule   greater than operator (>)
+ rule   less than or equal to operator (<=)
+ rule   greater than or equal to operator (>=)
+ rule   equal to operator (==)
+ rule   not equal to operator (!=)
+ rule   strict equal to operator (===)
+ rule   strict not equal to operator (!==)
+*/
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+/*
+
+rule avoid the comparison of null & undefined
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+*/
---
+++ datatypes-summary.js
@@ -0,0 +1,41 @@
+/*
+
+    title         Primitive
+    
+    notes       7 typess : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+
+    title         Reference (Non Primitive)
+
+    notes       Array, Object, Function
+
+    ?           Is Javascript Dynamic Typed Language Or Static Typed Language?
+    >           Javascript is Dynamic Typed Language (No need to specify data type).
+*/
+
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = true
+const outsideTemp = null
+let userEmail
+
+const id  = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+const bigNumber = 1238752481278213123n
+
+const heros = ["Hulk", "Thor", "Captain America"]
+let myObj = {
+    name: "Umang",
+    age : 21
+}
+
+const myFunction = function() {
+    console.log("Hello World");
+}
+
+console.log(typeof heros);
